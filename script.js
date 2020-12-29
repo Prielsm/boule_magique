73635db8abfd9b30e12c6fce1731d1abc8707837
Priels Manon <manon.priels@gmail.com>
2020-12-29 19:27:59 +0100

    app finished

--- script.js
+++ script.js
@@ -6,4 +6,53 @@ import './styles.scss';
 const app = document.getElementById('app');
 
 // every files in "static" folder can be used directly like that
-app.innerHTML += '<img src="images/kitten.jpg" style="width:100px;" />';
+// app.innerHTML += '<img src="images/kitten.jpg" style="width:100px;" />';
+
+const inputTxt = document.getElementById('inputTxt');
+const billard = document.body.querySelector('.billard');
+const arrow = document.body.querySelector('.arrow');
+const cliquez = document.body.querySelector('.cliquez');
+const listeReponses = ["Vous pouvez compter dessus.", "OUI, sans aucune hésitation !", "Les astres disent que oui.", "Cela demande réflexion.", "Mais oui, c'est clair.", "Peut-être que oui, peut-être que non.", "Ca m'étonnerait.", "Alors là, absolument pas!", "Ne comptez pas dessus...", "Un jour peut-être."];
+const reponse = document.body.querySelector('.reponse');
+const reset = document.body.querySelector('.reset');
+
+function toRamdom(liste) {
+  const randomMots = liste[Math.floor(Math.random() * liste.length)];
+  return randomMots;
+}
+
+inputTxt.addEventListener('keyup', () => {
+  const txtValue = inputTxt.value;
+  if (txtValue.length > 10) {
+    billard.style.opacity = '1';
+    billard.style.cursor = 'pointer';
+    arrow.classList.remove('d-none');
+    cliquez.classList.remove('d-none');
+  } else {
+    billard.style.opacity = '0.5';
+    billard.style.cursor = 'default';
+    arrow.classList.add('d-none');
+    cliquez.classList.add('d-none');
+  }
+});
+
+billard.addEventListener('click', () => {
+  if (billard.style.opacity === '1') {
+    reponse.classList.remove('d-none');
+    reponse.innerHTML = toRamdom(listeReponses);
+    billard.style.opacity = '0.5';
+    billard.style.cursor = 'default';
+    arrow.classList.add('d-none');
+    cliquez.classList.add('d-none');
+    reset.classList.remove('d-none');
+    inputTxt.disabled = 'true';
+  }
+});
+
+reset.addEventListener('click', () => {
+  console.log('hi');
+  inputTxt.removeAttribute('disabled');
+  inputTxt.value = "";
+  reponse.classList.add('d-none');
+  reset.classList.add('d-none');
+});
